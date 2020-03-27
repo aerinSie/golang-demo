fa22837882fd9d135e89d2d93c7625170d74d2aa
aerin_hsieh <aerin_hsieh@paradise-soft.com.tw>
2020-03-27 17:32:21 +0800

    調整package位置

--- src/main.go
+++ src/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
-
 	"golang-demo/src/config"
 	"golang-demo/src/usermgmt" //golang-demo是 專案名稱
 )
@@ -25,24 +24,34 @@ func main() {
 	// 初始化引擎
 	config.Config(myenv)
 	engine := gin.Default()
-	// http://localhost:9205/swagger/index.html
-	engine.GET("/swagger/*any", ginSwagger.DisablingWrapHandler(swaggerFiles.Handler, "NAME_OF_ENV_VARIABLE"))
-	// 注册一个路由和处理函数
-	engine.Any("/", WebRoot)
-	engine.POST("/login", usermgmt.PostLogin)
-	user := engine.Group("user")
-	user.GET("/info", usermgmt.GetUserInfoByAccount)
-	// user.GET("/:userID", userhandler.GetUserInfo)
-	user.POST("/register", usermgmt.PostRegister)
-	user.PUT("/change-password", usermgmt.PutUserChangePassword)
+	initRoute(engine)
 
-	// 绑定端口，然后启动应用
 	// 本地 $go run main.go local
 	// 遠端 $go run main.go prd
 	engine.Run(":9205")
 }
 
+// initRoute is 轉發 main url
+func initRoute(engine * gin.Engine)  {
+	// http://localhost:9205/swagger/index.html
+	engine.GET("/swagger/*any", ginSwagger.DisablingWrapHandler(swaggerFiles.Handler, "NAME_OF_ENV_VARIABLE"))
+	// 注册一个路由和处理函数
+	engine.Any("/", webRoot)
+	engine.POST("/login", usermgmt.PostLogin)
+
+	userRoute(engine)
+}
+
 // WebRoot is ...
-func WebRoot(context *gin.Context) {
+func webRoot(context *gin.Context) {
 	context.String(http.StatusOK, "hello, world")
+}
+
+// userRoute is 轉發 user開頭的url
+func userRoute(engine * gin.Engine){
+	user := engine.Group("user")
+	user.GET("/info", usermgmt.GetUserInfoByAccount)
+	// user.GET("/:userID", userhandler.GetUserInfo)
+	user.POST("/register", usermgmt.PostRegister)
+	user.PUT("/change-password", usermgmt.PutUserChangePassword)
 }
